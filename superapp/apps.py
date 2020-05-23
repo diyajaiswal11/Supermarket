996bce387947bbec916cf9c282cf665624383114
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-05-24 00:21:30 +0530

    home page

---
+++ superapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SuperappConfig(AppConfig):
+    name = 'superapp'
--- supermarket/settings.py
+++ supermarket/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'supermarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
