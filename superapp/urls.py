a730f77590ddca7e816c74386150d3626554de3a
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-05-24 22:29:07 +0530

    login page

--- superapp/urls.py
+++ superapp/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+#django.contrib.auth.views.LoginView
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('loginpage/',views.loginpage, name='loginpage'),
+    path('logoutpage/',views.logoutpage, name='logoutpage'),
+    path('register/',views.register,name='register'),
+    path('frontpage/',views.frontpage,name='frontpage'),
+    #url('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
+    #url('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- superapp/views.py
+++ superapp/views.py
@@ -1,6 +1,77 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
 # Create your views here.
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+from .forms import CreateUserForm, UserLoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login , logout, get_user_model
+from django.http import HttpResponseBadRequest, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+# Create your views here.
+
+
+def register(request):
+    user=request.user
+    if user.is_authenticated:
+        return redirect('home') 
+    else:
+        if request.method == 'POST':
+            form=CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request,'Account was created for '+ user)
+                return redirect('loginpage')
+        else:
+            form=CreateUserForm()    
+        context = {'form':form }    
+        return render(request,'register.html',context)
+
+def loginpage(request):
+    user = request.user
+    if user.is_authenticated:
+        return redirect('frontpage') 
+    else:
+        next = request.GET.get('next')
+        form = UserLoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            
+            login(request, user)
+            if next:
+                return redirect(next)
+            return redirect('frontpage')
+        
+        context = {
+            'form': form,
+        }
+        return render(request, 'loginpage.html', context)
+
+
+
+
+def logoutpage(request):
+    logout(request)
+    return redirect('home')
+
+
+
+
+
+
+
+
+
 
 def home(request):
     return render(request,'home.html')
+
+
+def frontpage(request):
+    return render(request,'frontpage.html')
+    
